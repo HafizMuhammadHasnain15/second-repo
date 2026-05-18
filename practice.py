afae7b70b78c5247566694828729c8c092a315a5
hafiz_hasnain_mughal <hafizmuhammadhasnain12@gmail.com>
2026-05-18 23:16:56 +0500

    fix bug

--- practice.py
+++ practice.py
@@ -6,11 +6,11 @@ name=input("enter your name : ")
 print(name)
 
 while True:
-    enter=input("if intersted to start again print enter otherwise print no ")
     number1=int(input("enter the first value : "))
     number2=int(input("enter the second  value : "))
     print(f"the addition of {number1} + {number2} is  {number1+number2}")
     print(f"the subtraction of {number1} - {number2} is  {number1-number2}")
     print(f"the multiplication of {number1} * {number2} is  {number1*number2}")
+    enter=input("if intersted to start again print enter otherwise print no ")
     if enter=="no":
         break
