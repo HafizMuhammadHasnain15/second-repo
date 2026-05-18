28240a88be98a58943914d97fc2cb37b13f7fb60
hafiz_hasnain_mughal <hafizmuhammadhasnain12@gmail.com>
2026-05-18 23:06:45 +0500

    add readme file

--- practice.py
+++ practice.py
@@ -4,3 +4,13 @@ print(name:=input("enter your name"))
 # simple input 
 name=input("enter your name : ")
 print(name)
+
+while True:
+    enter=input("if intersted to start again print enter otherwise print no ")
+    number1=int(input("enter the first value : "))
+    number2=int(input("enter the second  value : "))
+    print(f"the addition of {number1} + {number2} is  {number1+number2}")
+    print(f"the subtraction of {number1} - {number2} is  {number1-number2}")
+    print(f"the multiplication of {number1} * {number2} is  {number1*number2}")
+    if enter=="no":
+        break
